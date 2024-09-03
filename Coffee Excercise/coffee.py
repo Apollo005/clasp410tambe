3e803245412ec4da6f6132fc51d4eb3f25bba0dc
Aditya <adityamtambe@gmail.com>
2024-09-03 11:04:24 -0400

    update coffee.py

--- Coffee Excercise/coffee.py
+++ Coffee Excercise/coffee.py
@@ -9,6 +9,11 @@ This file solves Newton's law of cooling for ...
 '''
 
 import numpy as np
+import matplotlib.pyplot as plt
+
+# Set matplotlib stylesheet
+
+
 
 # Define constants for our problem
 
@@ -22,5 +27,38 @@ def solve_temp(time, k=k, T_init=T_init, T_env=T_env):
 
     return T_env + (T_init - T_env) * np.exp(-k*time)
 
-print(solve_temp(np.array([0,10])))
+def time_to_temp(T_target, k=k, T_init=T_init, T_env=T_env):
+    '''
+    '''
+
+    return (-1/k) * np.log((T_target - T_env)/(T_init - T_env))
+
+
+# Set up time array
+time = np.arange(0, 600, 1)
+
+# Get temperatrure for the three scenarios
+
+# Scen 1: No cream until 60 deg
+temp_scn1 = solve_temp(time)
+time_scn1 = time_to_temp(60.0)
+
+# Scen 2: Add cream immediately
+temp_scn2 = solve_temp(time, T_init=85)
+time_scn2 = time_to_temp(60.0, T_init=85)
+
+# plot below:
+
+fig, ax = plt.subplots(1,1)
+
+# Add temp vs time:
+ax.plot(time, temp_scn1, label = 'Scenario 1')
+ax.plot(time, temp_scn2, label = 'Scenario 2')
+
+#
+ax.axvline(time_scn1, ls = '--', c = 'C0', label =r'$t(T=60^{\circ})$')
+ax.axvline(time_scn2, ls = '--', c ='C1', label =r'$t(T=60^{\circ})$')
+
+ax.legend(loc = 'best')
+plt.show()
     
