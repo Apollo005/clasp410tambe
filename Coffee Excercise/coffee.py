2ccadbb3f67ecba1b3276f03167c482ee85c3b27
Aditya <adityamtambe@gmail.com>
2024-09-13 21:11:00 -0400

    update

--- Coffee Excercise/coffee.py
+++ Coffee Excercise/coffee.py
@@ -37,7 +37,7 @@ def time_to_temp(T_target, k=k, T_init=T_init, T_env=T_env):
 # Set up time array
 time = np.arange(0, 600, 1)
 
-# Get temperatrure for the three scenarios
+# Get temperature for the three scenarios
 
 # Scen 1: No cream until 60 deg
 temp_scn1 = solve_temp(time)
@@ -62,3 +62,47 @@ ax.axvline(time_scn2, ls = '--', c ='C1', label =r'$t(T=60^{\circ})$')
 ax.legend(loc = 'best')
 plt.show()
     
+
+    # #this function description, later p_bare, p_spread
+    # #1) create forest
+    # forest = np.zeros((maxiter,nNorth,nEast), dtype=int) + 2
+    # istart, jstart = nNorth//2, nEast//2
+    # forest[0, istart, jstart] = 3
+
+    # ignite = np.random.rand(nNorth, nEast)
+
+    # prob_spread = 1.0 # Chance to spread to adjacent cells.
+    # prob_bare = 0.0 # Chance of cell to start as bare patch.
+    # prob_start = 0.0 # Chance of cell to start on fire.
+
+    # fig,ax = plt.subplots(1,1)
+    # contour = ax.matshow(forest[0,:,:], vmin=1, vmax=3)
+    # ax.set_title(f'iteration = {0:03d}')
+    # plt.colorbar(contour, ax=ax)
+    # fig.savefig(f'fig{0:04d}.png')
+
+    # for k in range(maxiter-1):
+    #     forest[k+1, :, :] = forest[k, :, :]
+
+    #     for i in range(nNorth):
+    #         for j in range(nEast):
+    #             if forest[k, i, j] == 3:
+    #                 if i < nNorth - 1 and forest[k, i + 1, j] == 2:
+    #                     forest[k+1, i + 1, j] = 3
+    #                 if i > 0 and forest[k, i - 1, j] == 2:
+    #                     forest[k+1, i - 1, j] = 3
+    #                 if j < nEast - 1 and forest[k, i, j + 1] == 2:
+    #                     forest[k+1, i, j + 1] = 3
+    #                 if j > 0 and forest[k, i, j - 1] == 2:
+    #                     forest[k+1, i, j - 1] = 3
+
+    #     wasburn = forest[k, :, :] == 3
+    #     forest[k+1, wasburn] = 1
+
+    #     fig, ax = plt.subplots(1, 1)
+    #     contour = ax.matshow(forest[k+1, :, :], vmin=1, vmax=3)
+    #     ax.set_title(f'iteration = {k+1:03d}')
+    #     plt.colorbar(contour, ax=ax)
+    #     plt.show()
+    #     fig.savefig(f'fig{k+1:04d}.png')
+    #     plt.close('all')
--- Lab01/wildfire.py
+++ Lab01/wildfire.py
@@ -10,9 +10,7 @@ import matplotlib.pyplot as plt
 # # 1 - barren, 2 - forest, 3 - fire
 # nstep = 4 # Number of iterations
 # nx, ny = 3, 3 # Number of cells in X and Y direction.
-# prob_spread = 1.0 # Chance to spread to adjacent cells.
-# prob_bare = 0.0 # Chance of cell to start as bare patch.
-# prob_start = 0.0 # Chance of cell to start on fire.
+
 
 # # Create an initial grid, set all values to "2". dtype sets the value
 # # type in our array to integers only.
@@ -43,12 +41,24 @@ nNorth = 3
 nEast = 3
 maxiter = 4
 
+prob_spread = 1.0 # Chance to spread to adjacent cells.
+prob_bare = 1.0 # Chance of cell to start as bare patch.
+prob_start = 1.0 # Chance of cell to start on fire.
                 
-def fire_spread(nNorth = nNorth, nEast = nEast, maxiter = maxiter):
+def full_fire_spread(nNorth = nNorth, nEast = nEast, maxiter = maxiter, prob_spread = prob_spread,
+                      prob_bare = prob_bare, prob_start = prob_start):
     #this function description, later p_bare, p_spread
     #1) create forest
     forest = np.zeros((maxiter,nNorth,nEast), dtype=int) + 2
-    forest[0, 1, 1] = 3
+    istart, jstart = nNorth//2, nEast//2
+
+    isbare = np.random.rand(nNorth, nEast)
+    isbare = isbare <= prob_bare
+    forest[0,isbare] = 1
+    #random probability to start a fire
+    
+    if (np.random.rand() <= prob_start and forest[0, istart, jstart] != 1):
+        forest[0, istart, jstart] = 3
 
     fig,ax = plt.subplots(1,1)
     contour = ax.matshow(forest[0,:,:], vmin=1, vmax=3)
@@ -56,20 +66,25 @@ def fire_spread(nNorth = nNorth, nEast = nEast, maxiter = maxiter):
     plt.colorbar(contour, ax=ax)
 
     for k in range(maxiter-1) :
+        ignite = np.random.rand(nNorth, nEast)
 
         forest[k+1,:,:] = forest[k,:,:]
 
-        for i in range(nNorth):
-            for j in range(nEast):
-                if forest[k, i, j] == 3:
-                    if i < nNorth - 1 and forest[k, i + 1, j] == 2:
-                        forest[k+1, i + 1, j] = 3
-                    if i > 0 and forest[k, i - 1, j] == 2:
-                        forest[k+1, i - 1, j] = 3
-                    if j < nEast - 1 and forest[k, i, j + 1] == 2:
-                        forest[k+1, i, j + 1] = 3
-                    if j > 0 and forest[k, i, j - 1] == 2:
-                        forest[k+1, i, j - 1] = 3
+       # North
+        doburnN = (forest[k, :-1, :] == 3) & (forest[k, 1:, :] == 2) & (ignite[1:, :] <= prob_spread)
+        forest[k+1, 1:, :][doburnN] = 3
+
+        # South
+        doburnS = (forest[k, 1:, :] == 3) & (forest[k, :-1, :] == 2) & (ignite[:-1, :] <= prob_spread)
+        forest[k+1, :-1, :][doburnS] = 3
+
+        # East
+        doburnE = (forest[k, :, :-1] == 3) & (forest[k, :, 1:] == 2) & (ignite[:, 1:] <= prob_spread)
+        forest[k+1, :, 1:][doburnE] = 3
+
+        # West
+        doburnW = (forest[k, :, 1:] == 3) & (forest[k, :, :-1] == 2) & (ignite[:, :-1] <= prob_spread)
+        forest[k+1, :, :-1][doburnW] = 3
 
         wasburn = forest[k,:,:] == 3
         forest[k+1, wasburn] = 1
@@ -78,6 +93,26 @@ def fire_spread(nNorth = nNorth, nEast = nEast, maxiter = maxiter):
         contour = ax.matshow(forest[k+1,:,:], vmin=1, vmax=3)
         ax.set_title(f'iteration = {k+1:03d}')
         plt.colorbar(contour, ax=ax)
-        plt.show()
 
-fire_spread(3,3,4)
+        fig.savefig(f'fig{k:04d}.png')
+        plt.show()
+        plt.close('all')
+
+        #quit once all fires are out
+        nBurn = (forest[k+1,:,:] == 3).sum()
+        if nBurn == 0:
+            print(f"burn completed in {k+1} steps")
+            break
+
+    return k+1
+
+def explore_burnrate():
+    prob = np.arrange(0,1,.05)
+    nsteps = np.zeros(prob.size)
+    for i,p in enumerate(prob):
+        print(f"Burning for pspread = {p}")
+        nsteps[i] = full_fire_spread()
+
+    plt.plot(prob,nsteps)
+
+full_fire_spread(3,5,6)
