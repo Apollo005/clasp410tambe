537e1ab1e9d474604c66a785448fcc2521860f98
Aditya <adityamtambe@gmail.com>
2024-09-26 10:12:32 -0400

    update

--- Coffee Excercise/coffee.py
+++ Coffee Excercise/coffee.py
@@ -1,108 +1,166 @@
 #!/usr/bin/env python3
 
 '''
-This file produces the solution to the coffee problem which is really important to everyone.
+This file produces the solution to the coffe problem which is really
+important to everyone.
 
 To use this file....
 
 This file solves Newton's law of cooling for ...
+
+The following table sets the values and their units used throughout this code:
+
+| Symbol | Units  | Value/Meaning                                            |
+|--------|--------|----------------------------------------------------------|
+|T(t)    | C or K | Surface temperature of body in question                  |
+|T_init  | C or K | Initial temperature of body in question                  |
+|T_env   | C or K | Temperature of the ambient environment                   |
+|k       | 1/s    | Heat transfer coefficient                                |
+|t       | s      | Time in seconds
 '''
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Set matplotlib stylesheet
+# Set MPL style sheet
+plt.style.use('fivethirtyeight')
 
+# Define constants for our problem.
+k = 1/300  # Coeff. of cooling in units of 1/s.
+T_init, T_env = 90, 20  # initial and environmental temps in units of C.
 
-
-# Define constants for our problem
-
-k = 1/300 # Coefficient of cooling in units of 1/s
-T_init, T_env = 90, 20 # Initial and Environmental in units of C.
 
 def solve_temp(time, k=k, T_init=T_init, T_env=T_env):
-    '''    
-    Given an array of times in seconds, calculate temperature using Newton's Law of Cooling.
+    '''
+    For a given scalar or array of times, `t`, return the analytic solution
+    for Newton's law of cooling:
+
+    $T(t)=T_env + \left( T(t=0) - T_{env} \right) e^{-kt}$
+
+    ...where all values are defined in the docstring for this module.
+
+    Parameters
+    ==========
+    time : Numpy array
+        Array of times, in seconds, for which solution will be provided.
+
+    Other Parameters
+    ================
+    k : float
+        Heat transfer coefficient, defaults to 1/300. s^-1
+    T_env : float
+        Ambient environment temperature, defaults to 20°C.
+    T_init : float
+        Initial temperature of cooling object/mass, defaults to °C
+
+    Returns
+    =======
+    temp : numpy array
+        An array of temperatures corresponding to `t`.
     '''
 
     return T_env + (T_init - T_env) * np.exp(-k*time)
 
-def time_to_temp(T_target, k=k, T_init=T_init, T_env=T_env):
+
+def time_to_temp(T_target, k=1/300., T_env=20, T_init=90):
     '''
+    Given an initial temperature, `T_init`, an ambient temperature, `T_env`,
+    and a cooling rate, return the time required to reach a target temperature,
+    `T_target`.
+
+        Parameters
+    ==========
+    T_target : scalar or numpy array
+        Target temperature in °C.
+
+
+    Other Parameters
+    ================
+    k : float
+        Heat transfer coefficient, defaults to 1/300. s^-1
+    T_env : float
+        Ambient environment temperature, defaults to 20°C.
+    T_init : float
+        Initial temperature of cooling object/mass, defaults to °C
+
+    Returns
+    =======
+    t : scalar or numpy array
+        Time in s to reach the target temperature(s).
     '''
 
     return (-1/k) * np.log((T_target - T_env)/(T_init - T_env))
 
 
-# Set up time array
+def newtcool(t, T0):
+    '''Return dT/dt at time t using Newton's law of cooling'''
+    return -k * (T0 - T_env)
+
+
+def euler(diffq, f0, dt, tmax=600.):
+    '''
+    Given a function representing the first derivative of f, an
+    initial condiition f0 and a timestep, dt, solve for f using
+    Euler's method.
+    '''
+
+    # Initialize!
+    t = np.arange(0.0, tmax+dt, dt)
+    f = np.zeros(t.size)
+    f[0] = f0
+
+    # Integrate!
+    for i in range(t.size-1):
+        f[i+1] = f[i] + dt * diffq(t[i], f[i])
+
+    # Return values to caller:
+    return t, f
+
+
+def solve_rk8(diffq, f0, dt, tmax=600.):
+    '''
+    Solve a single ODE using the Dormand Prince 8th order adaptive RK
+    method with dense output (using Scipy's `solve_ivp` function).
+
+    Arguments are the same as `euler` and yes I'm too lazy to retype this
+    right now. No judgement please.
+    '''
+
+    from scipy.integrate import solve_ivp
+
+    result = solve_ivp(diffq, [0, tmax], [f0], method='DOP853')
+
+    # Return time, function values to caller:
+    return result.t, result.y[0, :]
+
+
+# Set up time array.
 time = np.arange(0, 600, 1)
 
-# Get temperature for the three scenarios
+# Get temperature for first scenario: no cream until 60 deg.
+temp_scn1 = solve_temp(time)     # Temp vs time
+time_scn1 = time_to_temp(60.0)   # Time to reach 60C from T_init
 
-# Scen 1: No cream until 60 deg
-temp_scn1 = solve_temp(time)
-time_scn1 = time_to_temp(60.0)
+# Repeat for scenario 2: add cream immediately.
+temp_scn2 = solve_temp(time, T_init=85)     # Temp vs time
+time_scn2 = time_to_temp(60.0, T_init=85)   # Time to reach 60C from T_init
 
-# Scen 2: Add cream immediately
-temp_scn2 = solve_temp(time, T_init=85)
-time_scn2 = time_to_temp(60.0, T_init=85)
+# Get numerical approx:
+time_euler, temp_euler = euler(newtcool, 90.0, dt=60)
+time_rk8, temp_rk8 = solve_rk8(newtcool, 90.0, dt=60)
 
-# plot below:
+# Plot! :)
+fig, ax = plt.subplots(1, 1)
 
-fig, ax = plt.subplots(1,1)
+# Add temp vs. time
+ax.plot(time, temp_scn1, label='Scenario 1')
+ax.plot(time, temp_scn2, label='Scenario 2')
+ax.plot(time_euler, temp_euler, label='Euler Approx')
+ax.plot(time_rk8, temp_rk8, label='DOP853 Approx')
 
-# Add temp vs time:
-ax.plot(time, temp_scn1, label = 'Scenario 1')
-ax.plot(time, temp_scn2, label = 'Scenario 2')
+# Add vert lines:
+ax.axvline(time_scn1, ls='--', c='C0', label=r'$t(T=60^{\circ})$')
+ax.axvline(time_scn2, ls='--', c='C1', label=r'$t(T=60^{\circ})$')
 
-#
-ax.axvline(time_scn1, ls = '--', c = 'C0', label =r'$t(T=60^{\circ})$')
-ax.axvline(time_scn2, ls = '--', c ='C1', label =r'$t(T=60^{\circ})$')
-
-ax.legend(loc = 'best')
-plt.show()
-    
-
-    # #this function description, later p_bare, p_spread
-    # #1) create forest
-    # forest = np.zeros((maxiter,nNorth,nEast), dtype=int) + 2
-    # istart, jstart = nNorth//2, nEast//2
-    # forest[0, istart, jstart] = 3
-
-    # ignite = np.random.rand(nNorth, nEast)
-
-    # prob_spread = 1.0 # Chance to spread to adjacent cells.
-    # prob_bare = 0.0 # Chance of cell to start as bare patch.
-    # prob_start = 0.0 # Chance of cell to start on fire.
-
-    # fig,ax = plt.subplots(1,1)
-    # contour = ax.matshow(forest[0,:,:], vmin=1, vmax=3)
-    # ax.set_title(f'iteration = {0:03d}')
-    # plt.colorbar(contour, ax=ax)
-    # fig.savefig(f'fig{0:04d}.png')
-
-    # for k in range(maxiter-1):
-    #     forest[k+1, :, :] = forest[k, :, :]
-
-    #     for i in range(nNorth):
-    #         for j in range(nEast):
-    #             if forest[k, i, j] == 3:
-    #                 if i < nNorth - 1 and forest[k, i + 1, j] == 2:
-    #                     forest[k+1, i + 1, j] = 3
-    #                 if i > 0 and forest[k, i - 1, j] == 2:
-    #                     forest[k+1, i - 1, j] = 3
-    #                 if j < nEast - 1 and forest[k, i, j + 1] == 2:
-    #                     forest[k+1, i, j + 1] = 3
-    #                 if j > 0 and forest[k, i, j - 1] == 2:
-    #                     forest[k+1, i, j - 1] = 3
-
-    #     wasburn = forest[k, :, :] == 3
-    #     forest[k+1, wasburn] = 1
-
-    #     fig, ax = plt.subplots(1, 1)
-    #     contour = ax.matshow(forest[k+1, :, :], vmin=1, vmax=3)
-    #     ax.set_title(f'iteration = {k+1:03d}')
-    #     plt.colorbar(contour, ax=ax)
-    #     plt.show()
-    #     fig.savefig(f'fig{k+1:04d}.png')
-    #     plt.close('all')
+ax.legend(loc='best')
+fig.savefig('coffee_solution.png')
